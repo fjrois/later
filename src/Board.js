b21a0282d976a6cca135911eb85eca80d93027b4
fjrois <fjroissiso@gmail.com>
2022-09-23 16:52:14 -0700

    Minor Board build fixand update

--- src/Board.js
+++ src/Board.js
@@ -1,28 +1,33 @@
 import './Board.css';
+import Card from './Card';
 
-const cards = [
-  {
-    id: '1',
-    summary: 'Do something with something',
-  },
-  {
-    id: '2',
-    summary: 'Do something with something',
-  },
-  {
-    id: '3',
-    summary: 'Do something with something',
-  },
-  {
-    id: '4',
-    summary: 'Do something with something',
-  },
-];
+// const cards = [
+//   {
+//     id: '1',
+//     summary: 'Do something with something',
+//   },
+//   {
+//     id: '2',
+//     summary: 'Do something with something',
+//   },
+//   {
+//     id: '3',
+//     summary: 'Do something with something',
+//   },
+//   {
+//     id: '4',
+//     summary: 'Do something with something',
+//   },
+// ];
 
 export default function Board() {
   return (
     <div className="board">
-      <p>Content</p>
+      {/* <p>Content</p> */}
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
