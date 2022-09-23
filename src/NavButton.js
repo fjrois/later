9aa614ea114d4c5cbd6dee3fe33b1aefbd400d64
fjrois <fjroissiso@gmail.com>
2022-09-23 16:54:22 -0700

    Minor NavButton build fix

--- src/NavButton.js
+++ src/NavButton.js
@@ -3,7 +3,8 @@ import './NavButton.css';
 export default function NavButton({ children }) {
   return (
     <button class="btn">
-      <a href="#">{children}</a>
+      {/* <a href="">{children}</a> */}
+      <div>{children}</div>
     </button>
   );
 }
